817d1a4bbf8f1777c5e0364b0fb21035f3e48164
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-06-01 16:34:19 +0100

    add prettier

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ module.exports = {
   images: {
     domains: [
       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-      "0.gravatar.com",
-      "1.gravatar.com",
-      "2.gravatar.com",
-      "secure.gravatar.com",
-      "i0.wp.com",
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+      'i0.wp.com',
     ],
   },
 };
