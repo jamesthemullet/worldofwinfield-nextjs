a12e85878cb3d395f13c741dff172add9c661d66
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-12-19 18:10:10 +0000

    remove posts

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,17 +2,17 @@ import { useRouter } from 'next/router';
 import ErrorPage from 'next/error';
 import Head from 'next/head';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import Container from '../../components/container';
-import PostBody from '../../components/post-body';
-import MoreStories from '../../components/more-stories';
-import PostHeader from '../../components/post-header';
-import SectionSeparator from '../../components/section-separator';
-import Layout from '../../components/layout';
-import PostTitle from '../../components/post-title';
-import Tags from '../../components/tags';
-import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api';
-import { CMS_NAME } from '../../lib/constants';
-import { PostProps } from '../../lib/types';
+import Container from '../components/container';
+import PostBody from '../components/post-body';
+import MoreStories from '../components/more-stories';
+import PostHeader from '../components/post-header';
+import SectionSeparator from '../components/section-separator';
+import Layout from '../components/layout';
+import PostTitle from '../components/post-title';
+import Tags from '../components/tags';
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../lib/api';
+import { CMS_NAME } from '../lib/constants';
+import { PostProps } from '../lib/types';
 
 export default function Post({ post, posts, preview }: PostProps) {
   const router = useRouter();
@@ -71,7 +71,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const allPosts = await getAllPostsWithSlug();
 
   return {
-    paths: allPosts.edges.map(({ node }) => `/posts/${node.slug}`) || [],
+    paths: allPosts.edges.map(({ node }) => `/${node.slug}`) || [],
     fallback: true,
   };
 };
