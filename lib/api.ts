4fdbca5fdb2f3275a200d209aab0b91a4ea40900
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-01-31 11:32:41 +0000

    change api used for slug

--- lib/api.ts
+++ lib/api.ts
@@ -215,141 +215,6 @@ export async function getAllPostsForHome(preview) {
   return data?.posts;
 }
 
-export async function getPostAndMorePosts(slug, preview, previewData) {
-  const postPreview = preview && previewData?.post;
-  // The slug may be the id of an unpublished post
-  const isId = Number.isInteger(Number(slug));
-  const isSamePost = isId ? Number(slug) === postPreview.id : slug === postPreview.slug;
-  const isDraft = isSamePost && postPreview?.status === 'draft';
-  const isRevision = isSamePost && postPreview?.status === 'publish';
-  const data = await fetchAPI(
-    `
-    fragment AuthorFields on User {
-      name
-      firstName
-      lastName
-      avatar {
-        url
-      }
-    }
-    fragment PostFields on Post {
-      title
-      excerpt
-      slug
-      date
-      featuredImage {
-        node {
-          mediaDetails {
-            sizes {
-              height
-              width
-              sourceUrl
-            }
-            height
-            width
-          }
-          srcSet
-          sourceUrl
-        }
-      }
-      author {
-        node {
-          ...AuthorFields
-        }
-      }
-      categories {
-        edges {
-          node {
-            name
-          }
-        }
-      }
-      tags {
-        edges {
-          node {
-            name
-          }
-        }
-      }
-      seo {
-        opengraphTitle
-        opengraphDescription
-        opengraphSiteName
-        opengraphImage {
-          uri
-          altText
-          mediaDetails {
-            file
-            height
-            width
-          }
-          mediaItemUrl
-          sourceUrl
-          srcSet
-        }
-      }
-    }
-    query PostBySlug($id: ID!, $idType: PostIdType!) {
-      post(id: $id, idType: $idType) {
-        ...PostFields
-        content
-        ${
-          // Only some of the fields of a revision are considered as there are some inconsistencies
-          isRevision
-            ? `
-        revisions(first: 1, where: { orderby: { field: MODIFIED, order: DESC } }) {
-          edges {
-            node {
-              title
-              excerpt
-              content
-              author {
-                node {
-                  ...AuthorFields
-                }
-              }
-            }
-          }
-        }
-        `
-            : ''
-        }
-      }
-      posts(first: 3, where: { orderby: { field: DATE, order: DESC } }) {
-        edges {
-          node {
-            ...PostFields
-          }
-        }
-      }
-    }
-  `,
-    {
-      variables: {
-        id: isDraft ? postPreview.id : slug,
-        idType: isDraft ? 'DATABASE_ID' : 'SLUG',
-      },
-    }
-  );
-
-  // Draft posts may not have an slug
-  if (isDraft) data.post.slug = postPreview.id;
-  // Apply a revision (changes in a published post)
-  if (isRevision && data.post.revisions) {
-    const revision = data.post.revisions.edges[0]?.node;
-
-    if (revision) Object.assign(data.post, revision);
-    delete data.post.revisions;
-  }
-
-  // Filter out the main post
-  data.posts.edges = data.posts.edges.filter(({ node }) => node.slug !== slug);
-  // If there are still 3 posts, remove the last one
-  if (data.posts.edges.length > 2) data.posts.edges.pop();
-
-  return data;
-}
-
 export async function getPage(id, idType = 'DATABASE_ID') {
   const data = await fetchAPI(
     `
@@ -384,6 +249,51 @@ export async function getPage(id, idType = 'DATABASE_ID') {
     }
   );
   return data.page;
+}
+
+export async function getPost(id, idType = 'SLUG') {
+  console.log(30, id);
+  const data = await fetchAPI(
+    `
+    query Post($id: ID!, $idType: PostIdType!) {
+      post(id: $id, idType: $idType) {
+        slug
+        content
+        title
+        date
+        seo {
+          metaKeywords
+          opengraphTitle
+          opengraphDescription
+          opengraphSiteName
+          opengraphImage {
+            uri
+            altText
+            mediaDetails {
+              file
+              height
+              width
+            }
+            mediaItemUrl
+            sourceUrl
+            srcSet
+          }
+        }
+        tags {
+          edges {
+            node {
+              name
+            }
+          }
+        }
+      }
+    }`,
+    {
+      variables: { id, idType },
+    }
+  );
+  console.log(40, data);
+  return data.post;
 }
 
 export async function getPostDisplayInfo(ids, idType = 'DATABASE_ID') {
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -9,7 +9,7 @@ import SectionSeparator from '../components/section-separator';
 import Layout from '../components/layout';
 import PostTitle from '../components/post-title';
 import Tags from '../components/tags';
-import { getAllPostsWithSlug, getPostAndMorePosts } from '../lib/api';
+import { getAllPostsWithSlug, getPost } from '../lib/api';
 import { CMS_NAME } from '../lib/constants';
 import { PostProps } from '../lib/types';
 import PrePost from '../components/pre-post';
@@ -54,14 +54,12 @@ export default function Post({ post, preview }: PostProps) {
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({ params, preview = false, previewData }) => {
-  const data = await getPostAndMorePosts(params?.slug, preview, previewData);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const data = await getPost(params?.slug);
 
   return {
     props: {
-      preview,
-      post: data.post,
-      posts: data.posts,
+      post: data,
     },
     revalidate: 10,
   };
