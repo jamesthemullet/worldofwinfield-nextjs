a78d7599d3a61ed1a18841fecefd3331a79e37ff
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-05-11 11:35:23 +0100

    get 404 page working

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import ErrorPage from 'next/error';
+
 import Head from 'next/head';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Container from '../components/container';
@@ -13,12 +13,13 @@ import { getAllPostsWithSlug, getPost } from '../lib/api';
 import { CMS_NAME } from '../lib/constants';
 import { PostProps } from '../lib/types';
 import PrePost from '../components/pre-post';
+import Custom404 from './404';
 
 export default function Post({ post, preview }: PostProps) {
   const router = useRouter();
 
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />;
+    return <Custom404 />;
   }
 
   return (
