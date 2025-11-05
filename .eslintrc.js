6ebe02df182b9394d26ef198ebf3fb287b82aefb
James Winfield <hello@jameswinfield.co.uk>
2025-11-05 16:13:40 +0000

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   overrides: [],
+  ignorePatterns: ['next-env.d.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
