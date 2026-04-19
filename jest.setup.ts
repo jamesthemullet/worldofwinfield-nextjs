aff3fcb5c1d7ca0882da16188b8789d685ef95f8
James Winfield <hello@jameswinfield.co.uk>
2026-04-19 08:34:53 +0100

    Update jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListenerListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    addEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
