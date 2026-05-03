82a4896bc17777b518de31f7856e7ceb578d2089
James Winfield <hello@jameswinfield.co.uk>
2026-05-03 10:15:45 +0100

    fix images

--- next.config.js
+++ next.config.js
@@ -19,8 +19,8 @@ const nextConfig = {
       { protocol: 'https', hostname: 'secure.gravatar.com' },
       { protocol: 'https', hostname: 'i0.wp.com' },
     ],
-    formats: ['image/avif', 'image/webp'],
-    qualities: [50, 75, 85],
+    formats: ['image/webp'],
+    minimumCacheTTL: 2592000,
   },
   env: {
     WORDPRESS_API_URL: process.env.WORDPRESS_API_URL,
