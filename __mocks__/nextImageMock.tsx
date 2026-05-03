7b379bb5fc77618b08358066cb36182f71d9592d
James Winfield <hello@jameswinfield.co.uk>
2026-05-03 10:40:12 +0100

    lint

--- __mocks__/nextImageMock.tsx
+++ __mocks__/nextImageMock.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Image = ({ src, alt, ...props }: React.ImgHTMLAttributes<HTMLImageElement> & { src: string; alt: string }) => (
-  // eslint-disable-next-line @next/next/no-img-element
   <img src={src} alt={alt} {...props} />
 );
 
