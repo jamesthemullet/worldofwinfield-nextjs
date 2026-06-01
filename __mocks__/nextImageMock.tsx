4048226796e7bcb55057630c65d29ec3e85cd971
James Winfield <hello@jameswinfield.co.uk>
2026-06-01 13:37:53 +0100

    fix mock

--- __mocks__/nextImageMock.tsx
+++ __mocks__/nextImageMock.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Image = ({
   src,
   alt,
+  priority,
   ...props
-}: React.ImgHTMLAttributes<HTMLImageElement> & { src: string; alt: string }) => (
-  <img src={src} alt={alt} {...props} />
+}: React.ImgHTMLAttributes<HTMLImageElement> & { src: string; alt: string; priority?: boolean }) => (
+  <img src={src} alt={alt} {...(priority ? { fetchpriority: 'high' } : {})} {...props} />
 );
 
 export default Image;
